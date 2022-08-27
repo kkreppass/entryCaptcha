358a1764cea7fcc61dc992e1af2d6c4a01367e6a
atobe <allang081108@gmail.com>
2022-08-27 22:20:54 +0900

    init

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors');
+let csrf = '';
+let body = '';
+async function getCookie ()  {
+  csrf = await fetch("https://playentry.org");
+  body = await csrf.text();
+  console.log(1)
+}
+getCookie()
+.then(()=>{
+console.log(2)
+const cookie = csrf.headers.get('set-cookie');
+console.log(cookie);
+const index = body.indexOf("csrf-token") + 21;
+const csrfToken = body.slice(index, index + 36);
+const app = express();
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+app.get('/api/:id', (req, res) => {
+fetch("https://playentry.org/graphql", {
+  "headers": {
+    "content-type": "application/json",
+    "CSRF-Token": `${csrfToken}`,
+    "cookie": `${cookie}`
+  },
+  "body": `{\"query\":\"\\n    query FIND_USERSTATUS_BY_USERNAME($id: String) {\\n        userstatus(id: $id) {\\n            id\\n            nickname\\n            username\\n            description\\n            shortUrl\\n            profileImage {\\n                \\n    id\\n    name\\n    label {\\n        \\n    ko\\n    en\\n    ja\\n    vn\\n\\n    }\\n    filename\\n    imageType\\n    dimension {\\n        \\n    width\\n    height\\n\\n    }\\n    trimmed {\\n        filename\\n        width\\n        height\\n    }\\n\\n            }\\n            coverImage {\\n                \\n    id\\n    name\\n    label {\\n        \\n    ko\\n    en\\n    ja\\n    vn\\n\\n    }\\n    filename\\n    imageType\\n    dimension {\\n        \\n    width\\n    height\\n\\n    }\\n    trimmed {\\n        filename\\n        width\\n        height\\n    }\\n\\n            }\\n            role\\n            studentTerm\\n            status {\\n                project\\n                projectAll\\n                study\\n                studyAll\\n                community {\\n                    qna\\n                    tips\\n                    free\\n                }\\n                following\\n                follower\\n                bookmark {\\n                    project\\n                    study\\n                }\\n                userStatus\\n            }\\n        }\\n    }\\n\",\"variables\":{\"id\":\"${req.params.id}\"}}`,
+  "method": "POST",
+})
+  .then(d=>d.text())
+  .then(d=>res.json(JSON.parse(d)))
+})
+app.listen(3000, () => {
+  console.log('server running!');
+})
+})
