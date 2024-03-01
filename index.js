45f9567257afff1dccd039db71095512e1dd0bfd
kkreppass <93640494+kkreppass@users.noreply.github.com>
2024-03-02 00:30:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,43 +3,57 @@ import fetch from 'node-fetch'
 import cors from 'cors'
 let csrf = '';
 let body = '';
-async function getCookie ()  {
+async function getCookie() {
   csrf = await fetch("https://playentry.org");
   body = await csrf.text();
 }
 getCookie()
-.then(()=>{
-const cookie = csrf.headers.get('set-cookie');
-console.log(cookie);
-const index = body.indexOf("csrf-token") + 21;
-const csrfToken = body.slice(index, index + 36);
-const app = express();
-const corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200
-}
-app.use(cors(corsOptions));
+  .then(() => {
+    const cookie = csrf.headers.get('set-cookie');
+    console.log(cookie);
+    const index = body.indexOf("csrf-token") + 21;
+    const csrfToken = body.slice(index, index + 36);
+    const app = express();
+    const corsOptions = {
+      origin: '*',
+      optionsSuccessStatus: 200
+    }
+    app.use(cors(corsOptions));
 
-app.get('/', (req, res) => {
-  res.send("/profile/유저id 로 get 요청을 보내세요(json)")
-})
-app.get('/profile/:id', (req, res) => {
-fetch("https://playentry.org/graphql", {
-  "headers": {
-    "content-type": "application/json",
-    "CSRF-Token": `${csrfToken}`,
-    "cookie": `${cookie}`
-  },
-  "body": `{\"query\":\"\\n    query FIND_USERSTATUS_BY_USERNAME($id: String) {\\n        userstatus(id: $id) {\\n            id\\n            nickname\\n            username\\n            description\\n            shortUrl\\n            profileImage {\\n                \\n    id\\n    name\\n    label {\\n        \\n    ko\\n    en\\n    ja\\n    vn\\n\\n    }\\n    filename\\n    imageType\\n    dimension {\\n        \\n    width\\n    height\\n\\n    }\\n    trimmed {\\n        filename\\n        width\\n        height\\n    }\\n\\n            }\\n            coverImage {\\n                \\n    id\\n    name\\n    label {\\n        \\n    ko\\n    en\\n    ja\\n    vn\\n\\n    }\\n    filename\\n    imageType\\n    dimension {\\n        \\n    width\\n    height\\n\\n    }\\n    trimmed {\\n        filename\\n        width\\n        height\\n    }\\n\\n            }\\n            role\\n            studentTerm\\n            status {\\n                project\\n                projectAll\\n                study\\n                studyAll\\n                community {\\n                    qna\\n                    tips\\n                    free\\n                }\\n                following\\n                follower\\n                bookmark {\\n                    project\\n                    study\\n                }\\n                userStatus\\n            }\\n        }\\n    }\\n\",\"variables\":{\"id\":\"${req.params.id}\"}}`,
-  "method": "POST",
-})
-  .then(d=>d.text())
-  .then(d=>res.json(JSON.parse(d)))
-})
+    app.get('/', (req, res) => {
+      res.send("server is working as i think :)")
+    });
+
+    app.get('/captcha', (req, res) => {
+      fetch("https://playentry.org/graphql", {
+        "headers": {
+          "content-type": "application/json",
+          "CSRF-Token": `${csrfToken}`,
+          "cookie": `${cookie}`
+        },
+        "body": "{\"query\":\"\\n    query ($captchaType: String!) {\\n        getCaptchaData (captchaType: $captchaType) {\\n            \\n    status\\n    result\\n\\n        }\\n    }\\n\",\"variables\":{\"captchaType\":\"image\"}}",
+        "method": "POST",
+      }).then(d => d.text())
+        .then(d => res.json(JSON.parse(d)))//captcha 불러오기
+    });
+
+    app.get('/confirm', (req, res) => {
+      let captchaValue = req.query.v;
+      let captchaKey = req.query.k;
+      fetch("https://playentry.org/graphql", {
+        "headers": {
+          "content-type": "application/json",
+          "CSRF-Token": `${csrfToken}`,
+          "cookie": `${cookie}`
+        },
+        "body": `{\"query\":\"\\n    mutation (\\n        $captchaValue: String!\\n        $captchaKey: String!    \\n        $captchaType: String!\\n    ) {\\n        setSnsMemberCaptcha (\\n            captchaValue: $captchaValue\\n            captchaKey: $captchaKey\\n            captchaType: $captchaType\\n        ) {\\n            \\n    status\\n    result\\n\\n        }\\n    }\\n\",\"variables\":{\"captchaValue\":\"${captchaValue}\",\"captchaKey\":\"${captchaKey}\",\"captchaType\":\"image\"}}`,
+        "method": "POST",
+      }).then(d => d.text())
+        .then(d => res.json(JSON.parse(d)))//captcha 확인
+    });
 
 
-
-app.listen(3000, () => {
-  console.log('server running!');
-})
-})
+    app.listen(3000, () => {
+      console.log('server running!');
+    })
+  })
